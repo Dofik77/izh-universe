5bfe5a1d9ada50d41a8e64e97e984ab91e6ea9f4
Stanislav Horochavcev <khorochavcev@mail.ru>
2022-11-16 23:57:15 +0400

    3d screen
    mostly complete 3d screen

--- Assets/Scripts/ModelStorage.cs
+++ Assets/Scripts/ModelStorage.cs
@@ -26,8 +26,31 @@ namespace DefaultNamespace
             int modelId = argumentsHandlerModelData.GetArgs();
 
             GameObject model = models.Model[modelId];
-            model = Instantiate(model, ModelPos.position, Quaternion.identity);
-            model.transform.SetParent(parentToModel);
+            
+            switch (modelId)
+            {
+                case 0 :
+                    model = Instantiate(model, ModelPos.position, Quaternion.identity);
+                    model.transform.SetParent(parentToModel);
+                    break;
+                case 1 :
+                    model = Instantiate(model, ModelPos.position + new Vector3(20,0,26), Quaternion.Euler(-90,0,0));
+                    model.transform.SetParent(parentToModel);
+                    break;
+                case 2 :
+                    model = Instantiate(model, ModelPos.position, Quaternion.Euler(-90,0,0));
+                    model.transform.SetParent(parentToModel);
+                    break;
+                case 3 :
+                    model = Instantiate(model, ModelPos.position, Quaternion.Euler(-90,0,0));
+                    model.transform.SetParent(parentToModel);
+                    break;
+                
+                default:
+                    break;
+            }
+            
+            
             currentModel = model;
         }
 
