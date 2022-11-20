cecb6b3b86e86022edd229c17ead22a9fb26f3ef
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-11-20 17:18:07 +0300

    feat: zoom at point

--- Assets/Scripts/UserLocation/IzhUniverseLocationService.cs
+++ Assets/Scripts/UserLocation/IzhUniverseLocationService.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class IzhUniverseLocationService : MonoBehaviour
 {
     private OnlineMapsMarker userMarker;
+    private bool findOnce;
+    private bool isObserving = false;
+    private Vector2 position;
+    OnlineMapsLocationService locationService;
 
     private void Start()
     {
-        userMarker = OnlineMapsMarkerManager.CreateItem(new Vector2(0, 0), null, "User");
-
-        OnlineMapsLocationService locationService = OnlineMapsLocationService.instance;
+        position = new Vector2(53.198057f, 56.843916f);
+        locationService = OnlineMapsLocationService.instance;
 
         if (locationService == null)
         {
@@ -16,19 +19,23 @@ public class IzhUniverseLocationService : MonoBehaviour
             return;
         }
 
-        locationService.OnLocationChanged += OnLocationChanged;
-        locationService.OnCompassChanged += OnCompassChanged;
-        
-        #if (UNITY_EDITOR)
-        userMarker.position = new Vector2(53.223785f, 56.849894f);
-        #endif
+        ZoomAt(position, 15);
+    }
 
-        OnlineMaps.instance.SetPositionAndZoom(userMarker.position.x, userMarker.position.y, 12);
+    private void Update()
+    {
+        if (locationService.IsLocationServiceRunning() && !isObserving)
+        {
+            ObserveUser();
+            isObserving = true;
+        }
     }
 
     private void OnLocationChanged(Vector2 position)
     {
         userMarker.position = position;
+        if(findOnce)
+            ShowUser(userMarker);
         Redraw();
     }
 
@@ -43,4 +50,26 @@ public class IzhUniverseLocationService : MonoBehaviour
         OnlineMaps.instance.Redraw();
     }
 
+    private void ShowUser(OnlineMapsMarker userMarker)
+    {
+        OnlineMaps.instance.SetPositionAndZoom(userMarker.position.x, userMarker.position.y, 12);
+        findOnce = false;
+    }
+
+    private void ZoomAt(Vector2 position, int zoom)
+    {
+        OnlineMaps.instance.SetPositionAndZoom(position.x, position.y, zoom);
+    }
+
+    private void ObserveUser()
+    {
+        userMarker = OnlineMapsMarkerManager.CreateItem(new Vector2(0, 0), null, "User");
+        findOnce = true;
+        locationService.OnLocationChanged += OnLocationChanged;
+        locationService.OnCompassChanged += OnCompassChanged;
+
+#if (UNITY_EDITOR)
+        userMarker.position = new Vector2(53.223785f, 56.849894f);
+#endif
+    }
 }
